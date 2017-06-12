d76900a61d35f05c27b8b627f4d6242ab658a3ce
jimmy <jimmy@local.host>
2017-06-12 16:16:56 +0800

    rm -rf broadcom/ ctc/

--- hsl/broadcom/linux/L4/bcm_cap_show.h
+++
@@ -1,5 +0,0 @@
-#ifndef BCM_CAP_SHOW_H
-#define BCM_CAP_SHOW_H
-
-extern int hsl_msg_recv_cap_info_show(struct socket *sock, struct hal_nlmsghdr *hdr, char *msgbuf);
-#endif
--- hsl/ctc/linux/L4/mac_acl_build.h
+++
@@ -1,7 +0,0 @@
-#ifndef _MAC_ACL_BUILD_H
-#define _MAC_ACL_BUILD_H
-
-extern int hsl_msg_recv_ifp_mac_acl_group_build(struct socket *sock, struct hal_nlmsghdr *hdr, char *msgbuf);
-
-#endif
-
