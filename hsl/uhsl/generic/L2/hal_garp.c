936577ce653c4e7ed5c13393e154334f1a0c7293
jimmy <jimmy@local.host>
2017-06-12 16:15:06 +0800

    删除uhsl/generic

--- hsl/uhsl/generic/L2/hal_garp.c
+++
@@ -1,9 +0,0 @@
-/* Copyright (C) 2004-2017 SZFORWARD, Inc. All Rights Reserved. */
-#include "hal_types.h"
-#include "hal_garp.h"
-
-int hal_garp_set_bridge_type (char *bridge_name, unsigned long garp_type,
-                           int enable)
-{
-    return 0;
-}
