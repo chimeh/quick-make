3d6d412aaa8c40304344d5f619cf49d9c717cd2d
Jimmy <jimin.huang@nx-engine.com>
2020-05-13 13:29:14 +0800

    删除 hsl/ fcu/libhal/

--- hsl/uhsl/hal_hw_reg.h
+++
@@ -1,9 +0,0 @@
-/* Copyright (C) 2004-2011 IP Infusion, Inc. All Rights Reserved. */
-#ifndef _HAL_HW_REG_H_
-#define _HAL_HW_REG_H_
-
-int hal_hw_reg_get (u_int32_t reg_addr, struct hal_reg_addr *reg);
-int hal_hw_reg_set (u_int32_t reg_addr, u_int32_t value);
-int hal_hw_reg_parse (struct hal_nlmsghdr *h, void *data);
-
-#endif /*_HAL_HW_REG_H_*/
